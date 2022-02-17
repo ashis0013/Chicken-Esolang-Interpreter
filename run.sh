7e265dd00f754aee63f39c41cf8eb247f360b489
Ashis Paul <ashis.paul@compass.com>
2022-02-18 00:30:00 +0530

    script updated

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
-kotlinc Extensions.kt
-kotlinc -cp . Chicken.kt
-kotlin ChickenKt $1 $2
+File=ChickenKt.class
+if [ -f "$File" ]; then
+	kotlin ChickenKt $1 $2
+else
+	kotlinc Extensions.kt
+	kotlinc -cp . Chicken.kt
+	kotlin ChickenKt $1 $2
+fi
